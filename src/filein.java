47524d2033d96153d1d2c54a865de87f05884e24
PearlinSmily <pearlinjebasingh@gmail.com>
2024-01-05 12:01:01 +0530

    commit

--- src/filein.java
+++ src/filein.java
@@ -1,13 +1,13 @@
-import java.io.FileInputStream;
+import java.io.FileInputStream;                    //importing io packages
 public class filein {
     public static void main(String[] args)
     {
         try{
-            FileInputStream fin=new FileInputStream("D:filein.txt");
+            FileInputStream fin=new FileInputStream("D:filein.txt");     //creating file
             int i;
-            while ((i = fin.read()) != -1)
+            while ((i = fin.read()) != -1)              //condition to check whether the file is empty or not
                 System.out. println((char) i);
-            fin.close();
+            fin.close();                                //closing file
         }
         catch (Exception e) {
             System.out.println(e);
--- src/fileout.java
+++ src/fileout.java
@@ -2,7 +2,7 @@ import java.io.FileOutputStream;                           //io package
     public class fileout {
         public static void main(String[] args) {
             try{
-                FileOutputStream fout = new FileOutputStream("D:filein.txt");     //creating file
+                FileOutputStream fout = new FileOutputStream("D:fileout.txt");     //creating file
 
                 String s = "hai iam smily";
 
