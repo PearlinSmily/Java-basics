f5d8bcbc15227153ec86b81a64b58853fc012a84
PearlinSmily <pearlinjebasingh@gmail.com>
2024-01-05 13:16:23 +0530

    commit

---
+++ src/exception.java
@@ -0,0 +1,17 @@
+public class exception {
+    public static void main(String[] args)
+    {
+        int a=56;
+        int b=0;
+        try{
+            System.out.println(a/b);               /*we know that any number divided by 0 gives an error
+                                                     which terminate the program without executing the full program.
+                                                      so we throw it to the compiler*/
+        }
+        catch (Exception E) {
+            System.out.println("a/b is 0");        /*but the catch section catch that exception and print some
+                                                      temporary sentence to avoid the program to get teminated abornamlly*/
+        }
+        System.out.println("after executing the exception");
+    }
+}
