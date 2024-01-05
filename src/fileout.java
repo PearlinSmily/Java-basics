87479ef2aa67fd975649a7f32433bea8709d9018
PearlinSmily <pearlinjebasingh@gmail.com>
2024-01-05 12:07:24 +0530

    commit

--- src/fileout.java
+++ src/fileout.java
@@ -6,7 +6,7 @@ import java.io.FileOutputStream;                           //io package
 
                 String s = "hai iam smily";
 
-                byte b[] = s.getBytes();                   //to convert the string into binary values
+                byte b[] = s.getBytes();                   //to convert the string into binary values which is bytestream
                 fout. write(b);                            // writes the text
 
                 fout. close();                             //close the file
