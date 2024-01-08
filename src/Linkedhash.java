b2af460af7d76027fe1d2edb930adf53778c3305
PearlinSmily <pearlinjebasingh@gmail.com>
2024-01-08 16:53:07 +0530

    commit

--- src/Linkedhash.java
+++ src/Linkedhash.java
@@ -3,11 +3,11 @@ import java.util.*;
     public class Linkedhash {
         public static void main(String[] args) {
             LinkedHashSet<String> veg = new LinkedHashSet<String>();
-            veg .add("apple");
-            veg .add("mango");
-            veg .add("grapes");
-            veg .add("guva");
-            veg .add("pineapple");
+            veg .add("carrot");
+            veg .add("beetroot");
+            veg .add("potato");
+            veg .add("onion");
+            veg .add("beans");
             Iterator<String> a = veg .iterator();
             for (String i:veg)
             {
