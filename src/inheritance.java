eeeb29829d0d6c6215d45354aef1b9367814b353
PearlinSmily <pearlinjebasingh@gmail.com>
2024-01-09 17:10:51 +0530

    commit

--- src/inheritance.java
+++ src/inheritance.java
@@ -1,13 +1,13 @@
  class inheritance {                                                     //super class
     String studname ="smily";
-    int age =24;
+
     {
         System.out.println("---student details---");
     }
 }
 class details extends inheritance {                                          //subclass
     int total = 545;
-
+    int age =24;
     public static void main(String[] args) {
         details sd = new details();
         System.out.println(sd.studname + " " + sd.age + " " + sd.total);
